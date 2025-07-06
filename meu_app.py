1bf1e52bb3abf56c30740a589a9c69915bc7fb80
Leonel mota <106840769+leomotta35@users.noreply.github.com>
2025-07-06 13:01:07 -0300

    Update meu_app.py

--- meu_app.py
+++ meu_app.py
@@ -4,10 +4,10 @@ import pandas as pd
 st.set_page_config(page_title="Meu Site Streamlit")
 
 with st.container():
-    st.subheader("Meu primeiro site com o Streamlit")
+    st.subheader("Site com o Streamlit")
     st.title("Dashboard de Contratos")
     st.write("Informações sobre os contratos fechados pela Hash&Co ao longo de maio")
-    st.write("Quer aprender Python? [Clique aqui](https://www.hashtagtreinamentos.com/curso-python)")
+    st.write("Meu Likedin: [Clique aqui](https://www.linkedin.com/in/leonel-mota-a0656122a?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BdGxf96YvR0Ow4bxDtK4GNA%3D%3D)")
 
 
 @st.cache_data
