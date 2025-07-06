6da28103150634eee4d436ac54c6a8a63137262f
Leonel mota <106840769+leomotta35@users.noreply.github.com>
2025-07-06 13:50:23 -0300

    Add files via upload

--- 03.py
+++ 03.py
@@ -2,6 +2,17 @@ import streamlit as st
 #import pandas as pd
  
 st.write("""
-# My first app
-Hello *world!*
+# My Likendin: www.linkedin.com/in/leonel-mota-a0656122a
+
+# Meu GitHub: www.github.com/leonelmota
+         
+Sobre mim: *Profissional com experiência abrangente em Business Intelligence, análise financeira e gestão de projetos, especializado na criação e implementação de painéis gerenciais estratégicos. Habilidade comprovada no desenvolvimento de dashboards para tomada de decisão, com foco em análise de Finanças, Probabilidades e Estatísticas, Recursos Humanos, Localidade, Desempenho, Sazonalidade e Tendências para otimizar resultados em todas as áreas.
+
+Com expertise em extração, tratamento e análise de dados utilizando ferramentas como Python, Power BI, Excel, Power Query e Pivot, Big Query, SQL Server, Looker, CGP, Google Analytics, AWS e APIs. Demonstra proficiência em automação de processos através de Python, incluindo a criação de pipelines de extração de dados em APIs e criação de soluções automatizadas para baixa de inadimplência, envio de comunicações e atualização de status de clientes.
+
+Com sólida formação Pós graduado em Administração Financeira, Gestão de Tecnologia da Informação e Gestão Empresarial, alia o conhecimento técnico com visão estratégica de negócios. Comprometido com a inovação, busca a aplicação de novas tecnologias e metodologias para aprimorar processos e gerar valor para as organizações.
+
+Experiência em ambientes de trabalho dinâmicos, incluindo empresas como Loca9motos, Faculdade Anhanguera, Fidelity/Bradesco e IBGE, com atuação em Business iteligence, análise de negócios, suporte técnico e supervisão.
+
+Me adicione no WhatsApp: https://wa.me/qr/TJV4XXFUEFGFP1*
 """)
